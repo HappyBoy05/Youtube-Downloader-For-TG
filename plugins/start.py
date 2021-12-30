dbc0495ce69a8a931952e54c0594f53dde017e14
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-30 11:19:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,10 +6,10 @@ async def start(client, message):
     # return
     Alpha = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("📌️ KOT BOTS 🔎", url="https://t.me/KOT_BOTS")],
-        [InlineKeyboardButton("📌️ support 🔎", url="https://t.me/KOT_REPORS")]
+        [InlineKeyboardButton("📌️ HB4All 🔎", url="https://t.me/HB4All")],
+        [InlineKeyboardButton("📌️ HB4All Bot 🔎", url="https://t.me/HB4All_Bot")]
 
     ])
-    thumbnail_url = "https://telegra.ph/file/94b0207fe12540dd68b08.jpg"
+    thumbnail_url = "https://telegra.ph/file/21170a66aa4733a9a4c26.jpg"
     await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
     raise StopPropagation
